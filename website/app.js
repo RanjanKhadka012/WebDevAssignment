511ea39b4c9f30d33cf88920a713fc61a2c008ef
RanjanKhadka012 <ranjankhadka012@gmail.com>
2024-12-07 00:35:54 -0600

    made the landing page responsive

--- website/app.js
+++ website/app.js
@@ -4,8 +4,8 @@ window.onload = () => {
         setTimeout(() => {
             // Redirect after 3 seconds
             window.location.href = "../login/login.html";
-            // Store a flag in localStorage to indicate redirection has happened
-            sessionStorages.setItem('redirected', 'true');
+            // Store a flag in sessionStorage to indicate redirection has happened
+            sessionStorage.setItem('redirected', 'true');
         }, 3000);
     }
 };
